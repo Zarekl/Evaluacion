99360f8f037e48cb259eb23d4af58df48d9f8608
Zarekl <carolina.mendoza@trescloud.com>
2025-06-24 17:44:50 -0500

    [ADD][CM]Modulo test

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- encoding:utf-8 -*-
+{
+    'name': 'Modulo de Prueba',
+    'version': '1.1',
+    'category': 'Test',
+    'description': '''
+    ''',
+    'author': '',
+    'maintainer': '',
+    'website': '',
+    'summary': 'Módulo de prueba',
+    'license': 'OEEL-1',
+    'depends': [
+        
+    ],
+    'data': [
+        # security
+        # views
+        #'views/view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
